723becdd03060e1cd74f9c5704f8a0ae0272b5e0
moeheinlatt <baloot.mhl@gmail.com>
2022-07-31 22:04:03 +0630

    fixed error

--- app/Orchid/Layouts/ProductListLayout.php
+++ app/Orchid/Layouts/ProductListLayout.php
@@ -3,88 +3,86 @@
 namespace App\Orchid\Layouts;
 
 use App\Models\Product;
-use Orchid\Screen\Actions\Button;
 use Orchid\Screen\Actions\DropDown;
 use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Layouts\Table;
 use Orchid\Screen\TD;
 
-class ProductListLayout extends Table
-{
-    /**
-     * Data source.
-     *
-     * The name of the key to fetch it from the query.
-     * The results of which will be elements of the table.
-     *
-     * @var string
-     */
-    protected $target = 'products';
+class ProductListLayout extends Table {
+	/**
+	 * Data source.
+	 *
+	 * The name of the key to fetch it from the query.
+	 * The results of which will be elements of the table.
+	 *
+	 * @var string
+	 */
+	protected $target = 'products';
 
-    /**
-     * Get the table cells to be displayed.
-     *
-     * @return TD[]
-     */
-    protected function columns(): array
-    {
-        return [
-            TD::make('code', 'Code')->sort(),
+	/**
+	 * Get the table cells to be displayed.
+	 *
+	 * @return TD[]
+	 */
+	protected function columns(): array
+	{
+		return [
+			TD::make('code', 'Code')->sort(),
 
-            TD::make('name', 'Product Name')->sort()
-                ->render(function (Product $product) {
-                    return Link::make($product->name)
-                        ->route('platform.product.edit', $product);
-                }),
+			TD::make('name', 'Product Name')->sort()
+				->render(function (Product $product) {
+					return Link::make($product->name)
+						->route('platform.product.edit', $product);
+				}),
 
-            TD::make('category_code', 'Category Code')->sort()
-                ->render(function (Product $product) {
-                    return $product->category->code;
-                }),
+			TD::make('category_code', 'Category Code')->sort()
+				->render(function (Product $product) {
+					return $product->category->code;
+				}),
 
-            TD::make('category_id', 'Category')->sort()
-                ->render(function (Product $product) {
-                    return $product->category->name;
-                }),
+			TD::make('category_id', 'Category')->sort()
+				->render(function (Product $product) {
+					return $product->category->name;
+				}),
 
-            TD::make('buy_price', 'Buy Price')
-                ->sort(),
+			TD::make('buy_price', 'Buy Price')
+				->sort(),
 
-            TD::make('sale_price', 'Sale Price')
-                ->sort(),
+			TD::make('sale_price', 'Sale Price')
+				->sort(),
 
-            TD::make('quantity', 'Quantity')
-                ->sort(),
+			TD::make('quantity', 'Quantity')
+				->sort(),
 
-            TD::make('group_id', 'Group')->sort()
-                ->render(function (Product $product) {
-                    return $product->group->name;
-                }),
+			TD::make('group_id', 'Group')->sort()
+				->render(function (Product $product) {
+					return $product->group->name;
+				}),
 
-            // TD::make('created_at', 'Created'),
+			// TD::make('created_at', 'Created'),
 
-            // TD::make('updated_at', 'Last edited'),
+			// TD::make('updated_at', 'Last edited'),
 
-            TD::make(__('Actions'))
-                ->align(TD::ALIGN_CENTER)
-                ->width('100px')
-                ->render(function (Product $product) {
-                    return DropDown::make()
-                        ->icon('options-vertical')
-                        ->list([
+			TD::make(__('Actions'))
+				->align(TD::ALIGN_CENTER)
+				->width('100px')
+				->render(function (Product $product) {
+					return DropDown::make()
+						->icon('options-vertical')
+						->list([
 
-                            Link::make(__('Edit'))
-                                ->route('platform.product.edit', $product->id)
-                                ->icon('pencil'),
+							Link::make(__('Edit'))
+								->route('platform.product.edit', $product->id)
+								->icon('pencil'),
 
-                            // Button::make(__('Delete'))
-                            //     ->icon('trash')
-                            //     ->confirm(__('Are you sure?'))
-                            //     ->method('remove', [
-                            //         'id' => $product->id,
-                                ]),
-                        ]);
-                }),
-        ];
-    }
+							// Button::make(__('Delete'))
+							//     ->icon('trash')
+							//     ->confirm(__('Are you sure?'))
+							//     ->method('remove', [
+							//         'id' => $product->id,
+							//     ]),
+						]);
+				}),
+		];
+	}
 }
