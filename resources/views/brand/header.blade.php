28b0b3a799389dc8c3d425728a3c8055aa771f28
Baloot <baloot.mhl@gmail.com>
2022-09-27 20:14:55 +0630

    added zabyuaungpyae logo for branches

--- resources/views/brand/header.blade.php
+++ resources/views/brand/header.blade.php
@@ -1,18 +1,22 @@
 @push('head')
-    <link
-        href="{{ asset('custom/img/logo.png') }}"
-        id="favicon"
-        rel="icon"
-    >
+    @if(auth()->user()->id == 1)
+        <link href="{{ asset('custom/img/logo.png') }}" id="favicon" rel="icon">
+    @else
+        <link href="{{ asset('custom/img/zabyuaungpyae-logo.jpeg') }}" id="favicon" rel="icon">
+    @endif
 @endpush
 
 <p class="h2 n-m font-thin v-center">
     {{-- <x-orchid-icon path="screen-smartphone"/> --}}
-
+    
     <span class="m-l d-none d-sm-block">
         {{-- MaharShin
         <small class="v-top opacity">Beta</small> --}}
-        <img src="{{ asset('custom/img/logo.png') }}" alt="" style="height: 150px;width:auto;text-align: center;">
+        @if(auth()->user()->id == 1)
+            <img src="{{ asset('custom/img/logo.png') }}" alt="" style="height: 150px;width:auto;text-align: center;">
+        @else
+            <img src="{{ asset('custom/img/zabyuaungpyae-logo.jpeg') }}" alt="" style="height: 150px;width:auto;text-align: center;">
+        @endif
     </span>
 
 </p>
