37dd48e4aa273e71c80730cec0afcb53ae2532f3
Baloot <baloot.mhl@gmail.com>
2022-09-28 07:43:05 +0630

    rollbacked logo due to error

--- resources/views/brand/header.blade.php
+++ resources/views/brand/header.blade.php
@@ -1,9 +1,9 @@
 @push('head')
-    @if(auth()->user()->id == 1)
+    {{-- @if(auth()->user()->id == 1) --}}
         <link href="{{ asset('custom/img/logo.png') }}" id="favicon" rel="icon">
-    @else
+    {{-- @else
         <link href="{{ asset('custom/img/zabyuaungpyae-logo.jpeg') }}" id="favicon" rel="icon">
-    @endif
+    @endif --}}
 @endpush
 
 <p class="h2 n-m font-thin v-center">
@@ -12,11 +12,11 @@
     <span class="m-l d-none d-sm-block">
         {{-- MaharShin
         <small class="v-top opacity">Beta</small> --}}
-        @if(auth()->user()->id == 1)
+        {{-- @if(auth()->user()->id == 1) --}}
             <img src="{{ asset('custom/img/logo.png') }}" alt="" style="height: 150px;width:auto;text-align: center;">
-        @else
+        {{-- @else
             <img src="{{ asset('custom/img/zabyuaungpyae-logo.jpeg') }}" alt="" style="height: 150px;width:auto;text-align: center;border-radius:30%;">
-        @endif
+        @endif --}}
     </span>
 
 </p>
