44a0b53eff74083f73d44e065c8d243f0c766a64
Baloot <baloot.mhl@gmail.com>
2022-09-29 20:06:11 +0630

    successfully added dynamic branch logo

--- resources/views/vendor/platform/header.blade.php
+++ resources/views/vendor/platform/header.blade.php
@@ -1,19 +1,21 @@
 @push('head')
     <meta name="robots" content="noindex" />
-    <link
+    {{-- <link
           href="{{ route('platform.resource', ['orchid', 'favicon.svg']) }}"
           sizes="any"
           type="image/svg+xml"
           id="favicon"
           rel="icon"
-    >
+    > --}}
+    <link href="{{ asset('custom/img/zabyuaungpyae-logo.jpeg') }}" id="favicon" rel="icon">
 @endpush
 
 <div class="h2 fw-light d-flex align-items-center">
-   <x-orchid-icon path="orchid" width="1.2em" height="1.2em"/>
+   {{-- <x-orchid-icon path="orchid" width="1.2em" height="1.2em"/> --}}
 
     <p class="ms-3 my-0 d-none d-sm-block">
-        ORCHID
-        <small class="align-top opacity">Platform</small>
+        {{-- ORCHID
+        <small class="align-top opacity">Platform</small> --}}
+        <img src="{{ asset('custom/img/zabyuaungpyae-logo.jpeg') }}" alt="" style="height: 150px;width:auto;text-align: center;border-radius:25%;">
     </p>
 </div>
